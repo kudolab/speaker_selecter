0921366723d99fb0b3580f009546f0bac16050b9
neqonagomiya <neqofan@gmail.com>
2023-02-15 23:46:39 +0900

    removed unnecessary comments in ver3/app.py

--- ver3/app.py
+++ ver3/app.py
@@ -37,32 +37,17 @@ def put_speaker_num():
     #speaker_num = request.json["speaker_num"]
     speaker_num = request.json["speaker_num"]
     # Error process
-    """
-    check GPIO available
-    will TFMeasure send {"speaker_id" : 1}
-    if speaker_num not 0 or speaker_num > len(speakerBCM_list):
-    """
-    #if speaker_num < 1 or speaker_num > 19:
     if (speaker_num < 1) or (speaker_num > 18):
         msg = f"Error: speaker_num must be in 1-18, got: {speaker_num}"
         print(msg, file=sys.stderr)
         return jsonify({"message": msg}), 400
-    """
-    # speaker_num is speaker_id! So, this if_command is bat 
-    if not speaker_num in speakerBCM_list:
-        msg = f"Error: speaker_num must be in setting.toml, got: {speaker_num}"
-        print(msg, file=sys.stderr)
-        return jsonify({"message": msg}), 400
-    """
 
     # speaker select process
     print(f"Present speaker number ... {speaker_num}", file=sys.stderr)
     # init pin (BCM)
     toybox.init_BCM(speakerBCM_list)
     # Turn ON the GPIO pin use "GPIO.output(speakerBCM, 1)
-    #toybox.on_BCM(speakerBCM_list[speaker_num-1]) # !use this
     toybox.on_BCM(speakerBCM_list[toybox.ID2BCM(speaker_num)])
-    ##toybox.on_BCM(speaker_num) # !test
     # make "ON GPIO pin history .json file"
     toybox.save_json("speaker_nowON.json", request.json)
 
